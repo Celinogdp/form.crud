2fdd8808ec52f785cbf17b0a3cdf1a74d64cb2f4
KAUAN CELINO FARIA <kauan.faria01@etec.sp.gov.br>
2023-10-26 22:11:33 -0300

    texti

--- form.crud/Form2.cs
+++ form.crud/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace form.crud
 {
@@ -15,6 +16,33 @@ namespace form.crud
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection mySql = new MySqlConnection("server=Localhost;database=bd_update_1;uid=root;pwd=etec");
+            mySql.Open();
+            MySqlCommand comando = new MySqlCommand("INSERT INTO usuario (nome, RG, endereco, telefone) values ('"+ txtNome.Text + "','"+ txtRG.Text + "','"+ txtEndereco.Text +"','"+ txtTelefone.Text +"');", mySql);
+            comando.ExecuteNonQuery();
+
+            MessageBox.Show("Aluno registrado com sucesso!!");
+            txtNome.Text = "";
+            txtRG.Text = "";
+            txtEndereco.Text = "";
+            txtTelefone.Text = "";
+        }
+
+        private void btnRegistros_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+
+            this.Visible = false;
         }
     }
 }
